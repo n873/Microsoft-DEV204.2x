f272956cc370ea10689a28739d10d7ac55d54783
Nathan Joseph Alard <nathan.alard@hotmail.com>
2018-04-30 19:36:15 +0200

    mod 3 tut 1

---
+++ Microsoft-DEV204.2x/Module3_Tut1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace Module3_Tut1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader streamReaderObject = null;
+
+            try
+            {
+                streamReaderObject = new StreamReader("file1.txt");
+
+                string contents = streamReaderObject.ReadToEnd();
+
+                streamReaderObject.Close();
+
+                Console.WriteLine("The file has {0} text elements.", new StringInfo(contents).LengthInTextElements);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("The file cannot be found");
+            }
+            finally {
+                if (streamReaderObject != null)
+                {
+                    streamReaderObject.Close();
+                    streamReaderObject.Dispose();
+                }
+            }
+        }
+    }
+}
